b8b4ad60f84d0aa7c3c3e1a39babd3a21a5e3710
Richard Jean-Baptiste <Richinbk1@gmail.com>
2019-11-05 21:02:27 -0500

    added chat functionality

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = process.env.port || 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.listen(port, () => {console.log("listening on port 3000")});
+io.on('connection', function(socket){
+    // send messages to chat
+   socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+   });
+   // send message when user connects
+   io.on('connection', function(socket){
+        io.emit('connected', 'user connected');
+   });
+
+  });
+
+http.listen(port, () => {console.log("listening on port 3000")});
